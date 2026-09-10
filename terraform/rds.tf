d03a5b680e2f02b06650415ed449058e13e0a397
Onl1H4z3 <aron83353@gmail.com>
2026-09-10 02:10:21 -0300

    fix

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -58,7 +58,7 @@ variable "db_password" {
   description = "Contrasena del usuario maestro. Minimo 8 caracteres; RDS rechaza '/', '@', '\"' y el espacio."
   type        = string
   sensitive   = true
-  default     = "postgres"
+  default     = "Duoc2026Postgres"
 
   validation {
     condition = (
